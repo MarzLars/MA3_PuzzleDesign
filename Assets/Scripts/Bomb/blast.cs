ac02924eaff69e2c3a3d6565af1c29a9677cca80
MarzLars <lars_gustav@hotmail.com>
2024-03-11 08:33:43 +0100

    Following tut

---
+++ Assets/Scripts/Bomb/blast.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blast : MonoBehaviour
+{
+   protected bool letPlay = true;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("space"))
+         {
+             letPlay = !letPlay;
+         }
+     
+         if(!letPlay)
+         {
+             if(!gameObject.GetComponent<ParticleSystem>().isPlaying)
+             {
+                 gameObject.GetComponent<ParticleSystem>().Play();
+                 letPlay = !letPlay;
+             }
+         }else{
+             if(gameObject.GetComponent<ParticleSystem>().isPlaying)
+             {
+                 gameObject.GetComponent<ParticleSystem>().Stop();
+             }
+         }
+    }
+}
